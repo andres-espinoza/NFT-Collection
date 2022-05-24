b3e5f8c88385ec7a660d577b8be7454787281bc9
Andrés Espinoza D <andres.espinozad@mail.udp.cl>
2022-05-24 15:28:05 -0400

    First Approach to create variables of design

---
+++ src/components/NFTCard/design-variables.js
@@ -0,0 +1,13 @@
+const design = {
+    cardColor: '#d2d2d2',
+    fontColor : '#4b4b4b',
+    borderRadius: '0.3rem',
+    titleFont: 'Syncopate, sans-serif'
+}
+
+export const { 
+    cardColor,
+    fontColor,
+    borderRadius,
+    titleFont 
+} = design
